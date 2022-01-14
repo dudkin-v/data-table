16b6dcb0b57b2fd5fdb2b6305434d03055bba062
dudkin-v <dudkin31v@gmail.com>
2022-01-14 11:05:19 +0200

    changed the styling of the app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import './App.styles.css';
 
 const App = () => {
   return (
+    <div className='app'>
+      <h1 className='app-heading'>Data table</h1>
     <Table data={data}/>
+    </div>
   );
 }
 
