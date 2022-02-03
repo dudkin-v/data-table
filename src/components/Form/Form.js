d49ff4314c5556affdd072ab361562094912d58e
dudkin-v <dudkin31v@gmail.com>
2022-02-03 23:41:39 +0200

    replaced validation, did not use form library

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,5 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+
+import isEmail from 'validator/es/lib/isEmail';
+import isMobilePhone from 'validator/es/lib/isMobilePhone';
 
 import './Form.styles.css';
 
@@ -9,43 +12,102 @@ import './Form.styles.css';
     const [email, setEmail] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
 
+    const [nameDirty, setNameDirty] = useState(false);
+    const [surnameDirty, setSurnameDirty] = useState(false);
+    const [nameError, setNameError] = useState('Field cannot be empty');
+    const [surnameError, setSurnameError] = useState('Field cannot be empty');
+    const [emailError, setEmailError] = useState(false);
+    const [phoneNumberError, setPhoneNumberError] = useState(false);
+    const [formIsValid, setFormIsValid] = useState(false);
+
+    useEffect(() => {
+        nameError || surnameError || emailError || phoneNumberError ?
+            setFormIsValid(false)
+            : setFormIsValid(true);
+    }, [nameError, surnameError, emailError, phoneNumberError])
+
     const resetFields = () => {
         setName('');
         setSurname('');
         setEmail('');
         setPhoneNumber('');
+        setNameError('Field cannot be empty');
+        setSurnameError('Field cannot be empty');
+        setNameDirty(false);
+        setSurnameDirty(false);
+        setEmailError(false);
+        setPhoneNumberError(false);
     }
 
-    const handleSubmit = (event) => {
+    const onBlur = ({ target: {name} }) => {
+        switch (name) {
+            case 'name':
+                setNameDirty(true);
+                break
+            case 'surname':
+                setSurnameDirty(true);
+                break
+        }
+    }
+
+        const handleSubmit = (event) => {
         event.preventDefault();
 
-         onSubmit({
-          name: name,
-          surname: surname,
-          email: email,
-          phoneNumber: phoneNumber,
-          id: Date.now()
-        });
+            onSubmit({
+                name: name,
+                surname: surname,
+                email: email,
+                phoneNumber: phoneNumber,
+                id: Date.now()
+            });
 
-        resetFields();
+            resetFields();
+
     }
 
     const handleNameChange = ({ target: {value} }) => {
         setName(value.trim());
+        if(value.length < 2) {
+            setNameError('Enter 2 or more characters');
+            if(!value.length){
+                setNameError('Field cannot be empty');
+            }
+        } else {
+            setNameError('');
+        }
     }
     const handleSurnameChange = ({ target: {value} }) => {
         setSurname(value.trim());
+        if(value.length < 2) {
+            setSurnameError('Enter 2 or more characters');
+            if(!value.length){
+                setSurnameError('Field cannot be empty');
+            }
+        } else {
+            setSurnameError('');
+        }
     }
     const handleEmailChange = ({ target: {value} }) => {
         setEmail(value.trim());
+        if(!isEmail(value)) {
+            setEmailError(true);
+        } else {
+            setEmailError(false);
+        }
     }
     const handlePhoneNumberChange = ({ target: {value} }) => {
         setPhoneNumber(value.trim());
+        if(!isMobilePhone(value, 'uk-UA')) {
+            setPhoneNumberError(true);
+        } else {
+            setPhoneNumberError(false);
+        }
     }
 
     return (
           <div className='form-container'>
             <form onSubmit={handleSubmit}>
+                {(nameDirty && nameError) && <p className='error'>{nameError}</p>}
                <label htmlFor='name'>Name:</label>
                   <input 
                     type='text'
@@ -53,8 +115,10 @@ import './Form.styles.css';
                     id='name'
                     value={name}
                     onChange={handleNameChange}
+                    onBlur={onBlur}
                   />
-              
+
+                {(surnameDirty && surnameError) && <p className='error'>{surnameError}</p>}
                <label htmlFor='surname'>Surname:</label>
                   <input 
                     type='text'
@@ -62,8 +126,10 @@ import './Form.styles.css';
                     id='surname'
                     value={surname}
                     onChange={handleSurnameChange}
+                    onBlur={onBlur}
                   />
 
+                {phoneNumberError ? <p className='error'>Enter valid phone number</p> : ''}
                 <label htmlFor='phoneNumber'>Tel:</label>
                   <input 
                     type='text'
@@ -72,7 +138,8 @@ import './Form.styles.css';
                     value={phoneNumber}
                     onChange={handlePhoneNumberChange}
                   />
-              
+
+                {emailError ? <p className='error'>Enter valid email</p> : ''}
                 <label htmlFor='email'>Email:</label>
                   <input 
                     type='text'
@@ -83,7 +150,7 @@ import './Form.styles.css';
                   />
 
                 <div>
-                  <button className='add-btn' type='submit'>Add</button>
+                  <button className='add-btn' type='submit' disabled={!formIsValid}>Add</button>
                 </div>
 
             </form>
